fdb68976df4ab06f58396b506ab51f5de2aa9881
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-02-02 16:40:32 +0300

    Lab 1 [tasks 1,2]
    Лабораторная работа 1. Знакомство с Руби. Установка компилятора + Sublime Text. Первые шаги и начало лабораторки

---
+++ lab_1.rb
@@ -0,0 +1,23 @@
+#################################
+#			  LAB 1				#
+#################################
+
+# 1 #
+# Hello World in Ruby + Comment
+puts "Hello, slaves!"
+# puts - ставит в конце символ новой строки
+# print - не делает этого
+
+# 2 #
+# Welcome the user
+puts "What's your name? (⌐■_■)"
+
+input_name = gets
+
+puts "Ky-ky, #{input_name}"
+
+# 3 #
+# About language...
+puts "What's your favourite language (programming, of course)?"
+
+input_language = gets
