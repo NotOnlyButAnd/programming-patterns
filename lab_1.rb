e0ed5cb3b55b03e0d166352c16972dde6b0ed470
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-02-22 21:45:48 +0300

    Lab 1 [3.2]
    Рассмотрел оператор unless. Правда чтобы сделать задание придумал глупый костыль)

--- lab_1.rb
+++ lab_1.rb
@@ -23,17 +23,32 @@ puts "What's your favourite language (programming, of course)?"
 input_language = gets
 inp_down = input_language.chomp.downcase
 
-if inp_down == 'ruby' then
-	puts "Oh, what's a toady!"
-elsif inp_down == 'c#'
-	puts "Ruby will be soon... But Windows apps is cool"
-elsif inp_down == "java"
-	puts "Ruby will be soon... However, your pain is too hard"
-elsif inp_down == "sql"
-	puts "Ruby will be soon... And your database will die"
-else
-	puts "Ruby will be soon... And please, learn some other language"
-end
+# 3.1 #
+# if inp_down == 'ruby' then
+# 	puts "Oh, what's a toady!"
+# elsif inp_down == 'c#'
+# 	puts "Ruby will be soon... But Windows apps is cool"
+# elsif inp_down == "java"
+# 	puts "Ruby will be soon... However, your pain is too hard"
+# elsif inp_down == "sql"
+# 	puts "Ruby will be soon... And your database will die"
+# else
+# 	puts "Ruby will be soon... And please, learn some other language"
+# end
 
 # проверка "типа" объектов
 # puts "\nNOT downcase - #{input_language.class}, down - #{inp_down.class}"
+
+# 3.2 #
+unless inp_down != 'ruby' then
+	puts "Oh, what's a toady!"
+else
+	puts "Ruby will be soon... And please, learn some other language"
+	if 	inp_down == 'c#'
+		puts "/But Windows apps is cool/"
+	elsif inp_down == "java"
+ 		puts "/However, your pain is too hard/"
+	elsif inp_down == "sql"
+	 	puts "/And your database will die/"
+	end
+end
