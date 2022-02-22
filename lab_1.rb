554ffe525f5c4d8dc1398343bd174723eeee9e45
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-02-22 21:32:45 +0300

    Lab 1 [3.1]
    Ура! условный оператор для строк работает. Оказывается, в конце вводимой строки стоял перенос строки, который нужно убирать методом chomp, и уже потом сравнивать строки.

--- lab_1.rb
+++ lab_1.rb
@@ -21,3 +21,19 @@ puts "Ky-ky, #{input_name}"
 puts "What's your favourite language (programming, of course)?"
 
 input_language = gets
+inp_down = input_language.chomp.downcase
+
+if inp_down == 'ruby' then
+	puts "Oh, what's a toady!"
+elsif inp_down == 'c#'
+	puts "Ruby will be soon... But Windows apps is cool"
+elsif inp_down == "java"
+	puts "Ruby will be soon... However, your pain is too hard"
+elsif inp_down == "sql"
+	puts "Ruby will be soon... And your database will die"
+else
+	puts "Ruby will be soon... And please, learn some other language"
+end
+
+# проверка "типа" объектов
+# puts "\nNOT downcase - #{input_language.class}, down - #{inp_down.class}"
