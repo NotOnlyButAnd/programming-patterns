cf22bf0d076de087ae57be49d0a538382b8b0dcd
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-03-09 14:28:42 +0300

    Lab 1 [1v2]
    Изучил как работать с аргументами программы. И то, что метод gets вызывается как Kernel:gets, и пытается воспринять аргументы программы как пути к файлам. Чтобы этого не происходило а читалось именно из консоли, уточнил метод до STDIN.gets

--- lab_1.rb
+++ lab_1.rb
@@ -8,19 +8,30 @@ puts "Hello, slaves!"
 # puts - ставит в конце символ новой строки
 # print - не делает этого
 
-# 2 #
+# 2.1 #
+# Welcome the user by name from command line
+if ARGV.length == 1
+	puts "Ky-ky, #{ARGV[0]}"
+else
+	puts "Sorry, too much(less) arguments. Try to put only one."
+end
+
+puts "#{ARGV}"
+
+# 2.2 #
 # Welcome the user
-puts "What's your name? (⌐■_■)"
+# puts "What's your name? (⌐■_■)"
 
-input_name = gets
+# input_name = gets
 
-puts "Ky-ky, #{input_name}"
+# puts "Ky-ky, #{input_name}"
+
 
 # 3 #
 # About language...
 puts "\nWhat's your favourite language (programming, of course)?"
 
-input_language = gets
+input_language = STDIN.gets
 inp_down = input_language.chomp.downcase
 
 # 3.1 #
@@ -70,13 +81,13 @@ end
 # 4 #	 	
 # About RUBY command...
 puts "\nPlease, input some Ruby command"
-inp_ruby_com = gets.chomp
+inp_ruby_com = STDIN.gets.chomp
 puts "Result: "
 eval(inp_ruby_com)	# интрепретирует строку как код на Ruby, напр: puts "Hey you..."
 
 # About OS command...
 puts "\nPlease, input some OS command"
-inp_os_com = gets.chomp
+inp_os_com = STDIN.gets.chomp
 puts "Result: "
 exec inp_os_com	# выполняет команду системы, напр: help
 				# тут можно использовать system inp_os_com, вроде как она в подоболочке запустится
