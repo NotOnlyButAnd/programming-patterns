a9f6cb8780cff1aaa73a8cdca862e8db372d2833
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-03-16 14:17:52 +0300

    Lab 1.2 [2]
    Сделаны методы из второго задания второй части, правда самым простым и линейным образом)

--- lab_1_part_2.rb
+++ lab_1_part_2.rb
@@ -11,8 +11,8 @@ end
 puts "your number: #{input_number}"
 
 # sum of number digits
-num_copy = 125
-puts num_copy.class	# Number? Integer? Fixnum????
+num_copy = input_number
+# puts num_copy.class	# Number? Integer! Fixnum????
 sum_dig = 0
 while num_copy > 0
 	sum_dig += num_copy % 10
@@ -20,11 +20,11 @@ while num_copy > 0
 	num_copy /= 10
   #puts "новое число #{num_copy}"
 end
-puts sum_dig
+puts "Sum by code: ", sum_dig
 
 # 2 #
 # Finding sum of number digits
-def sum_of_digit number
+def sum_of_digits number
 	num_copy = number
 	sum_dig = 0
 	while num_copy > 0
@@ -34,7 +34,8 @@ def sum_of_digit number
 	return sum_dig
 end
 
-puts sum_of_digits 522
+num_copy = input_number
+puts "Sum by function: ", sum_of_digits(num_copy)
 
 # Finding max nujmber digit
 def max_digit number
@@ -50,4 +51,36 @@ def max_digit number
 	return max_dig
 end
 
-puts max_digit 522
+num_copy = input_number
+puts "Max digit by fun: ", max_digit(num_copy)
+
+# Finding min nujmber digit
+def min_digit number
+	num_copy = number
+	min_dig = 9
+	while num_copy > 0
+		cur_dig = num_copy % 10
+		if cur_dig < min_dig
+			min_dig = cur_dig
+		end
+		num_copy /= 10
+	end
+	return min_dig
+end
+
+num_copy = input_number
+puts "Min digit by fun: ", min_digit(num_copy)
+
+# Finding mul of number digits
+def mul_of_digits number
+	num_copy = number
+	mul_dig = 1
+	while num_copy > 0
+		mul_dig *= num_copy % 10
+		num_copy /= 10
+	end
+	return mul_dig
+end
+
+num_copy = input_number
+puts "Mul by function: ", mul_of_digits(num_copy)
