6751c4519807f75d566ba118dc6ad388597ef021
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-03-16 15:35:04 +0300

    Lab 1.2 [3.0]
    Задание 3 начато. Сделана функция с case, осталось запилить еще три метода отдельными коммитами

--- lab_1_part_2.rb
+++ lab_1_part_2.rb
@@ -2,6 +2,7 @@
 #	LAB 1 PART 2		#
 #################################
 # 1 #
+=begin
 if ARGV.length == 1
 	input_number = ARGV[0].to_i
 else
@@ -9,8 +10,10 @@ else
 end
 
 puts "your number: #{input_number}"
+=end
 
 # sum of number digits
+=begin
 num_copy = input_number
 # puts num_copy.class	# Number? Integer! Fixnum????
 sum_dig = 0
@@ -21,6 +24,7 @@ while num_copy > 0
   #puts "новое число #{num_copy}"
 end
 puts "Sum by code: ", sum_dig
+=end
 
 # 2 #
 # Finding sum of number digits
@@ -33,9 +37,6 @@ def sum_of_digits number
 	end
 	return sum_dig
 end
-
-num_copy = input_number
-puts "Sum by function: ", sum_of_digits(num_copy)
 
 # Finding max nujmber digit
 def max_digit number
@@ -51,9 +52,6 @@ def max_digit number
 	return max_dig
 end
 
-num_copy = input_number
-puts "Max digit by fun: ", max_digit(num_copy)
-
 # Finding min nujmber digit
 def min_digit number
 	num_copy = number
@@ -68,9 +66,6 @@ def min_digit number
 	return min_dig
 end
 
-num_copy = input_number
-puts "Min digit by fun: ", min_digit(num_copy)
-
 # Finding mul of number digits
 def mul_of_digits number
 	num_copy = number
@@ -82,5 +77,34 @@ def mul_of_digits number
 	return mul_dig
 end
 
-num_copy = input_number
-puts "Mul by function: ", mul_of_digits(num_copy)
+# 3 #
+# main func for task 3 part 2 lab 1
+def my_main number, num_meth
+	puts "your number: #{number}"
+	num_copy = number
+	case num_meth
+	when 1
+		print "Min digit by fun: ", min_digit(num_copy)
+	when 2
+		print "Max digit by fun: ", max_digit(num_copy)
+	when 3
+		print "Sum by function: ", sum_of_digits(num_copy)
+	when 4
+		print "Mul by function: ", mul_of_digits(num_copy)
+	else
+		print "Hey, i haven't such method :("
+	end
+end
+
+if ARGV.length == 2 then
+	# print "So... There are methods:\n1.Min digit\n2.Max digit\n3.Sum digits\n4.Mul digits\n"
+	my_main(ARGV[0].to_i, ARGV[1].to_i)
+elsif ARGV.length == 1	
+	puts "Sorry, too less arguments. Try to put 2 arguments."
+elsif ARGV.length == 0
+	puts "Hello world!"
+else
+	puts "Sorry, too much arguments. Try to put 2 arguments."
+end
+
+
