0b1780389e0d3def4519b21178303f0b97b85ed7
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-03-17 22:16:25 +0300

    Lab 1.2 [3.3]
    Найти сумму всех делителей числа, взаимно простых с суммой цифр числа и не взаимно простых с произведением цифр числа. Реализован НОД (алг-м Евклида. Понято, что делители включают и само число тоже

--- lab_1_part_2.rb
+++ lab_1_part_2.rb
@@ -13,6 +13,7 @@ puts "your number: #{input_number}"
 =end
 
 # sum of number digits
+# number_copy is USLESS!
 =begin
 num_copy = input_number
 # puts num_copy.class	# Number? Integer! Fixnum????
@@ -29,50 +30,46 @@ puts "Sum by code: ", sum_dig
 # 2 #
 # Finding sum of number digits
 def sum_of_digits number
-	num_copy = number
 	sum_dig = 0
-	while num_copy > 0
-		sum_dig += num_copy % 10
-		num_copy /= 10
+	while number > 0
+		sum_dig += number % 10
+		number /= 10
 	end
 	return sum_dig
 end
 
 # Finding max nujmber digit
 def max_digit number
-	num_copy = number
 	max_dig = 0
-	while num_copy > 0
-		cur_dig = num_copy % 10
+	while number > 0
+		cur_dig = number % 10
 		if cur_dig > max_dig
 			max_dig = cur_dig
 		end
-		num_copy /= 10
+		number /= 10
 	end
 	return max_dig
 end
 
 # Finding min nujmber digit
 def min_digit number
-	num_copy = number
 	min_dig = 9
-	while num_copy > 0
-		cur_dig = num_copy % 10
+	while number > 0
+		cur_dig = number % 10
 		if cur_dig < min_dig
 			min_dig = cur_dig
 		end
-		num_copy /= 10
+		number /= 10
 	end
 	return min_dig
 end
 
 # Finding mul of number digits
 def mul_of_digits number
-	num_copy = number
 	mul_dig = 1
-	while num_copy > 0
-		mul_dig *= num_copy % 10
-		num_copy /= 10
+	while number > 0
+		mul_dig *= number % 10
+		number /= 10
 	end
 	return mul_dig
 end
@@ -81,20 +78,21 @@ end
 # main func for task 3 part 2 lab 1
 def my_main number, num_meth
 	puts "your number: #{number}"
-	num_copy = number
 	case num_meth
 	when 1
-		print "Min digit by fun: ", min_digit(num_copy)
+		print "Min digit by fun: ", min_digit(number)
 	when 2
-		print "Max digit by fun: ", max_digit(num_copy)
+		print "Max digit by fun: ", max_digit(number)
 	when 3
-		print "Sum by function: ", sum_of_digits(num_copy)
+		print "Sum by function: ", sum_of_digits(number)
 	when 4
-		print "Mul by function: ", mul_of_digits(num_copy)
+		print "Mul by function: ", mul_of_digits(number)
 	when 5
-		print "Count of divider, that is not divide by 3: ", count_div_not_by_3(num_copy)
+		print "Count of divider, that is not divide by 3: ", count_div_not_by_3(number)
 	when 6
-		print "Min uneven (odd) digit: ", min_uneven_dig(num_copy)
+		print "Min uneven (odd) digit: ", min_uneven_dig(number)
+	when 7
+		print "Sum div, mutually simple with sum dig and !mut simple with mul dig: ", sum_div_mut_simp(number)
 	else
 		print "Hey, i haven't such method :("
 	end
@@ -108,6 +106,9 @@ def count_div_not_by_3 number
 		if number % div == 0 && div % 3 != 0
 			count += 1
 		end
+	end
+	if number % 3 != 0
+		count += 1
 	end
 	return count
 end
@@ -125,6 +126,34 @@ def min_uneven_dig number
 	else
 		return min
 	end
+end
+
+# Finding NOD num1 and num2
+def my_nod number1, number2
+	while number1 != 0 && number2 !=0
+		if number1 > number2
+			number1 %= number2
+		else
+			number2 %= number1
+		end
+	end
+	return number1 + number2
+end
+
+# Finding sum div, mutually simple with sum dig and !mut simple with mul dig
+def sum_div_mut_simp number
+	sum_div = 0
+	sum_dig = sum_of_digits(number)
+	mul_dig = mul_of_digits(number)
+	for div in 1..number/2
+		if my_nod(div, sum_dig) == 1 && my_nod(div, mul_dig) != 1 && number % div == 0
+			sum_div += div
+		end
+	end
+	if my_nod(number, sum_dig) == 1 && my_nod(number, mul_dig) != 1
+		sum_div += div
+	end
+	return sum_div
 end
 
 if ARGV.length == 2 then
