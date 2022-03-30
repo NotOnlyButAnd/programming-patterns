a7ee345bb9c9186e7a98a340ba1078b61e4e65af
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-03-30 12:43:24 +0300

    Lab 1.5.1 [1]
    Задание 1 + вызов метода

--- lab_1_p_5_1.rb
+++ lab_1_p_5_1.rb
@@ -17,6 +17,19 @@ def all_data my_str
     return my_str.scan(/([0-2]\d|3[0-1]) (января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря) (\d{4})/).map{|e| e.join(' ')}.join("; ")
 end
 
+# Variant 11. Tasks: 5, 7, 14
+
+# 5 #
+=begin
+Дана строка. Необходимо найти наибольшее количество идущих
+подряд символов кириллицы
+аФаЯсФasdA12иии*фыв(!"ФАФЯмяофл -> max = ФАФЯмяофл
+=end
+def task_1 my_str
+    return my_str.scan(/[а-яёА-ЯЁ]+/).max{|a,b| a.length <=> b.length }
+end
+
 puts "Input some string: "
 first_string = STDIN.gets.chomp
-puts "Date's: ", all_data(first_string)
+#puts "Date's: ", all_data(first_string)
+puts "Max kyrillic: #{task_1(first_string)}"
