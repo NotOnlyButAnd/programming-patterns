de6f54b359bb53769a8fbb32bdef19bf0aefca20
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-03-30 11:38:18 +0300

    Lab 1.5 [2]
    Метод + вызов для рандомной строки

--- lab_1_p_5.rb
+++ lab_1_p_5.rb
@@ -9,5 +9,16 @@ def task_1 my_str
 	return my_str.split('').sort_by{rand}.join
 end
 
+# 7 #
+=begin
+Дана строка, состоящая из символов латиницы. Необходимо проверить, 
+образуют ли прописные символы этой строки палиндром
+=end
+def task_2 my_str
+	t = my_str.scan(/[A-Z]/)
+	return t == t.reverse
+end
+
 first_str = "Привет мир!"
 puts "Shaked str: #{task_1(first_str)}"
+puts "Is palindrom? -> #{task_2("As We Can")}"
