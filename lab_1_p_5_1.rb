148274090b7bb6d159ae02dc9d4cf2a38e13b132
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-04-06 13:36:16 +0300

    Lab 2.1 [5]
    Добавил метод класса для проверки номера телефона. Заменил геттеры и сеттеры на акцессоры.

--- lab_1_p_5_1.rb
+++ lab_1_p_5_1.rb
@@ -1,68 +1,99 @@
-# XXX/ Этот код необходим только при использовании русских букв на Windows
-if (Gem.win_platform?)
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
+class Department
+	attr_accessor :name
+  	attr_reader :phone
 
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
+	def initialize (name, phone, *commitments)	# конструктор класса
+		@name = name	# поле объекта
+		self.phone = phone
+		@commitments = commitments
+		@selected_comm_index = 0
+	end
+
+	# плохой геттер, лучше не получать массив целиком
+	#def getCommitments	# Геттер на обязанности (целиком на массив)
+	#	@commitments
+	#end
+
+	def printInfo	# вывод информации об объекте класса Department
+		puts "Department \"#{@ame}\"; Contact phone: #{@phone}; Commitments: #{@commitments}"
+	end
+
+	def addCommitment(commitment)	# добавление обязанности
+		@commitments.append(commitment)
+	end
+
+	def selectCommitmentByName(commitment)	# Выбор обязанности по имени
+		@selected_comm_index = @commitments.index(commitment)
+	end
+
+	def selectCommitmentByIndex(commitment_index)	# Выбор обязанности по индексу
+		@selected_comm_index = commitment_index
+	end
+
+	def deleteCommitmentByPos(commitment_index)	# удаление обязанности по индексу
+		@commitments.delete_at(commitment_index)
+	end
+
+	def deleteCommitmentByName(commitment)	# удаление обязанности по названию
+		@commitments.delete(commitment)
+	end
+
+	def deleteSelectedCommitment	# удаление выделенной обязанности
+		@commitments.delete_at(@selected_comm_index)
+		# @selected_comm_index = 0
+	end
+
+	def getSelectedCommText		# получение текста выделенной обязанности
+		@commitments[@selected_comm_index]
+	end
+
+	def changeSelectedCommText(val)		# изменение текста выделенной обязанности
+		@commitments[@selected_comm_index] = val
+	end
+
+	def printCommitments	# вывод обязанностей на экран
+		puts "Commitments of \"#{@name}\":"
+		@commitments.each { |x| puts x}
+	end
+
+	# метод класса проверки корректности номера телефона (простая проверка в международном формате)
+	def self.isCorrectPhone (val)
+		/^((\+7)+([0-9]){10})$/.match(val).to_s == val
+	end
+
+	def phone=(val)
+		if self.class.isCorrectPhone(val)
+			@phone = val
+		else
+			raise ArgumentError.new("Phone number is incorrect!")
+		end
+	end
 end
-# /XXX
 
 =begin
-Дана строка. Необходимо найти все даты, которые описаны в
-виде "31 февраля 2007"
+# создаем объекты класса
+dep_1 = Department.new('First Department', '+79996332237')
+puts "#{dep_1.getName} with phone: #{dep_1.getPhone} were created..."
+# puts "#{dep_1.name} with phone: #{dep_1.phone} were created..." - так не получится
+
+dep_2 = Department.new('Second Department', '+78612541231')
+puts "#{dep_2.getName} with phone: #{dep_2.getPhone} were created..."
 =end
-def all_data my_str
-    return my_str.scan(/([0-2]\d|3[0-1]) (января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря) (\d{4})/).map{|e| e.join(' ')}.join("; ")
-end
 
-# Variant 11. Tasks: 5, 7, 14
+# создаем объект и выводим инфу более красиво
+dep_3 = Department.new('Third Department', '+78613991111')
+print "Were created: "
+dep_3.printInfo
+dep_3.addCommitment("Work for BOSS")
+dep_3.addCommitment("Sitting at the chair")
+dep_3.addCommitment("Drinking tea")
+dep_3.printCommitments
 
-# 5 #
-=begin
-Дана строка. Необходимо найти наибольшее количество идущих
-подряд символов кириллицы
-аФаЯсФasdA12иии*фыв(!"ФАФЯмяофл -> max = ФАФЯмяофл
-=end
-def task_1 my_str
-    return my_str.scan(/[а-яёА-ЯЁ]+/).max{|a,b| a.length <=> b.length }
-end
-
-# 7 #
-=begin
-Дана строка. Необходимо найти минимальное из имеющихся в ней
-натуральных чисел
-предположим, что раз НАТУРАЛЬНЫЕ ЧИСЛА, то разделены хоть чем-то (пробел)
-12 4 28 3 84 -> 3
-=end
-def task_2 my_str
-    return my_str.split(' ').min { |a, b| a.to_i <=> b.to_i }
-end
-
-# 14 #
-=begin
-Дана строка. Необходимо найти наибольшее количество идущих
-подряд цифр
-аФа1054ЯсФasdA12иии*ф_)*0019284ыв(!"ФАФЯмяофл -> 0019284
-=end
-def task_3 my_str
-    return my_str.scan(/[0-9]+/).max{|a,b| a.length <=> b.length }
-end
-
-puts "Input some string: "
-first_str = STDIN.gets.chomp
-puts "Choose task\n1 - Get all date's;\n2 - Get max kyrillic sequence\n3 - Get min natural numb\n4 - Get max num sequence"
-choice = STDIN.gets.chomp.to_i
-case choice
-    when 1
-        print "Date's: ", all_data(first_str)
-    when 2
-        print "Max kyrillic: ", task_1(first_str)
-    when 3
-        print "Min natural num: ", task_2(first_str)
-    when 4
-        print "Max num: ", task_3(first_str)
-    else
-        print "Hey, i haven't such method :("
-    end
+# создаем объект с обязанностями
+sales_dep = Department.new('Sales department', '+78612341122', 'Making reports', 'Realizating sales', 'Changing tariffs')
+sales_dep.printInfo
+sales_dep.selectCommitmentByIndex(1)
+sales_dep.changeSelectedCommText("Making paper planes")
+puts sales_dep.getSelectedCommText
+sales_dep.deleteSelectedCommitment
+sales_dep.printInfo
